77fbcdb91d5687fc579e8e093b53139817434074
root <root@ifchange-dev-110.(none)>
2016-08-16 11:13:30 +0800

    update code

--- example.php
+++ example.php
@@ -1,6 +1,7 @@
 <?php
-require './crontab.php';
-//print_r(get_mday_by_wday(array(1,2),8,2016));
-//exit;
+require './CrontabRule.php';
+
 echo date('Y-m-d H:i:s') . "\n";
-echo date('Y-m-d H:i:s', get_next_timestamp('* * */4 * 1,5')) . "\n";
+var_dump(CrontabRule::check_time_rule('*/2 * 3,6,9 * 1'));
+echo date('Y-m-d H:i:s', CrontabRule::get_next_time('*/5 * */4 * 1,5')) . "\n";
+echo date('Y-m-d H:i:s', CrontabRule::get_next_time('*/5 * */4 * 1,5', strtotime('2016-08-17 00:05:00'))) . "\n";
